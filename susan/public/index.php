08bbe072e0c6af9bd38354b8f840066b698e3616
jonathan <jonathan@dgngroep.nl>
2018-11-07 21:31:36 +0100

    Move all files in seperate folders to allow nginx to read them

--- susan/public/index.php
+++ susan/public/index.php
@@ -1,8 +1,60 @@
-<html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php echo '<p>Hello World</p>'; ?>
- </body>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Jonathan van der Ceelen">
+        <meta name="description" content="Susan Broers costumes">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Susan Broers | Costumes</title>
+        <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+        <link rel="stylesheet/less" href="/style/style.less">
+        <script type="text/javascript" src="/script/less.min.js"> </script>
+        <script type="text/javascript" src="/script/jquery-2.2.2.min.js"></script>
+        <!-- <script src="script.js" type="text/javascript"></script> -->
+    </head>
+    <body>
+        <div class="content-wrapper">
+            <header>
+                <img src="/image/logo.png" alt="Logo Susan Broers" />
+                <nav>
+                    <div class="nav-item">Home</div>
+                    <div class="nav-item">About</div>
+                    <div class="nav-item">Portfolio</div>
+                    <div class="nav-item">Blog</div>
+                    <div class="nav-item">Contact</div>
+                </nav>
+            </header>
+            <main>
+                <div class="category">
+                    <figure>
+                        <a href="#">
+                            <img src="/image/history.jpg" alt="History Costume by Susan Broers" />
+                            <figcaption>History</figcaption>
+                        </a>
+                    </figure>
+                </div>
+                <div class="category">
+                    <figure>
+                        <a href="#">
+                            <img src="/image/fantasy.jpg" alt="Fantasy Costume by Susan Broers" />
+                            <figcaption>Fantasy</figcaption>
+                        </a>
+                    </figure>
+                </div>
+                <div class="category">
+                    <figure>
+                        <a href="#">
+                            <img src="/image/vintage.jpg" alt="Vintage Costume by Susan Broers" />
+                            <figcaption>Vintage</figcaption>
+                        </a>
+                    </figure>
+                </div>
+            </main>
+            <footer>
+                <div class="footer-content">
+                    <p>Susan Broers</p>
+                </div>
+            </footer>
+        </div>
+    </body>
 </html>
